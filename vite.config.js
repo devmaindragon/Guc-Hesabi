668a9b4c9754fa61825c9881e482bd9022f8a73f
devmaindragon <dev.main.dragon@gmail.com>
2026-08-26 15:07:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // GitHub Pages alt dizinde yayınlanacaksa: base: "/guc-hesabi/"
-  base: "/guc-hesabi/",
+  base: "./guc-hesabi/",
 });
